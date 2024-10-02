40fcaf59d0cfe953ffeb338256d93f65d5920d6d
Bakhodir Toshmatov <64396506+Uzbek6565@users.noreply.github.com>
2024-10-02 12:43:40 +0500

    Update streamlit_app.py
    Formatting response

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,8 +35,10 @@ ingredients_list = st.multiselect(
     , my_dataframe
     , max_selections=5
 )
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
+
 if ingredients_list:
     st.write(ingredients_list)
     st.text(ingredients_list)
