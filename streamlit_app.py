ba75580b6bbb0f6aadb40fdb07e03f848a204505
Bakhodir Toshmatov <64396506+Uzbek6565@users.noreply.github.com>
2024-10-02 13:09:35 +0500

    Update streamlit_app.py
    updating dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,9 @@ st.write(
 cnx = st.connection('snowflake')
 # session = get_active_session()
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select('FRUIT_NAME')
-# st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 get_orders = """select * from smoothies.public.orders where ORDER_FILLED = 0"""
 
